c803851c6aa6cfdbbee71277624ad194a97f0c9a
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-03 11:07:21 +0300

    add ABAP sample project
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

---
+++ my-first-abap-project/src/run.mjs
@@ -0,0 +1,35 @@
+import { DatabaseInitializer } from "sdk/abap/database";
+import { initializeABAP } from '../dist/abap/init.mjs';
+
+import { zcl_abap_app } from '../dist/abap/zcl_abap_app.clas.mjs';
+
+async function initialize() {
+    DatabaseInitializer.initDefaultDataSource();
+    await initializeABAP();
+
+    // manually register the table
+    abap.DDIC.EMPLOYEES = {
+        "objectType": "TABL",
+        "type": {
+            "value": {
+                "offset": {
+                    "value": 0,
+                    "qualifiedName": "I"
+                },
+                "length": {
+                    "value": 0,
+                    "qualifiedName": "I"
+                }
+            },
+            "qualifiedName": "employees",
+            "ddicName": "employees",
+            "suffix": {},
+            "asInclude": {}
+        },
+        "keyFields": ["id"]
+    };
+}
+
+await initialize();
+
+await zcl_abap_app.run();
